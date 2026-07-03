485afaf2f721ec1588227d0525995ab5e36f19e1
co16661666 <62578553+co16661666@users.noreply.github.com>
2026-07-03 12:57:47 -0400

    Transferred over force code from ForceDataConversionScript.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+/*----- IMU Setup -----*/
 // Set the delay between fresh samples=
 #define BNO055_SAMPLERATE_DELAY_MS (10) // 100 Hz
 
@@ -23,14 +24,29 @@ struct __attribute__((__packed__)) CalibrationData
   uint8_t sys, gyro, accel, mag;
 };
 
+/*----- Force Setup -----*/
+// Define the analog pins based on the wiring table
+const int forceThumb  = A0;
+const int forceIndex  = A1;
+const int forceMiddle = A12;
+const int forceRing   = A13;
+const int forcePinky  = A4;
+
+// Array for easier iteration
+const int forcePins[] = {forceThumb, forceIndex, forceMiddle, forceRing, forcePinky};
+const int numFingers = 5;
+
 // Serial communication bits
-const byte START_BIT = 0xAA;
+const byte START_BIT_IMU = 0xAA;
+const byte START_BIT_FF = 0xBB;
 const byte END_BIT = 0x55;
 
 // State machine variable
 uint8_t state = 0;
-unsigned long imu_dt = 10000; // us
+unsigned long imu_dt = 10000; // us, 100 Hz
 unsigned long imu_prev_time = 0;
+unsigned long ff_dt = 1000; // us, 1000 Hz
+unsigned long ff_prev_time = 0;
 
 void setup(void)
 {
@@ -68,10 +84,15 @@ void loop(void)
           state = 1;
           imu_prev_time = micros();
         }
+        else if (micros() - ff_prev_time > ff_dt)
+        {
+          state = 3;
+          ff_prev_time = micros();
+        }
         break;
       }
 
-    case 1:
+    case 1: // Write IMU quaternion and acceleration data
       {
         imu::Quaternion quat = bno.getQuat();
         imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL); // Filtered acceleration data
@@ -86,14 +107,14 @@ void loop(void)
         imu_packet.ay = accel.y();
         imu_packet.az = accel.z();
 
-        Serial.write(START_BIT);
+        Serial.write(START_BIT_IMU);
         Serial.write((byte*)&imu_packet, sizeof(imu_packet)); // message
         Serial.write(END_BIT); // end of message
         
-        state = 0;
+        state = 3;
         break;
       }
-    case 2:
+    case 2: // Write IMU calibration status
       {
         uint8_t system, gyroscope, accelerometer, magnetometer;
         bno.getCalibration(&system, &gyroscope, &accelerometer, &magnetometer);
@@ -104,9 +125,22 @@ void loop(void)
         cal_packet.accel = accelerometer;
         cal_packet.mag = magnetometer;
 
-        Serial.write(START_BIT);
+        Serial.write(START_BIT_IMU);
         Serial.write((byte*)&cal_packet, sizeof(cal_packet)); // message
         Serial.write(END_BIT); // end of message
+
+        state = 0;
+        break;
+      }
+    case 3: // Write force data
+      {
+        Serial.write(START_BIT_FF);
+        // Loop through each pin, read the value, and print it
+        for (int i = 0; i < numFingers; i++) {
+          int sensorValue = analogRead(forcePins[i]);
+          Serial.write(sensorValue);
+        }
+        Serial.write(END_BIT);
 
         state = 0;
         break;
